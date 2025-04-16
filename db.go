395eee0a2f8b19640faa48a24e2a4e876d4091d1
kebukeyi <193276660@qq.com>
2025-04-16 14:20:46 +0800

    refactor(db): 简化 TrainKVDB 的 Set 方法
    - 移除了未使用的注释代码
    - 删除了冗余的错误处理和日志记录代码
    - 保留了设置条目到 LSM 的核心功能

--- db.go
+++ db.go
@@ -94,12 +94,6 @@ func (db *TrainKVDB) Get(key []byte) (*model.Entry, error) {
 }
 
 func (db *TrainKVDB) Set(entry *model.Entry) error {
-	//if entry.Key == nil || len(entry.Key) == 0 {
-	//	return common.ErrEmptyKey
-	//}
-	//entry.Key = model.KeyWithTs(entry.Key)
-	//entry.Version = model.ParseTsVersion(entry.Key)
-	//err := db.BatchSet([]*model.Entry{entry})
 	err := db.SetToLSM(entry)
 	return err
 }
